ff57992d2c662e58bc7f7dbc127c61078f446e94
Lucas Martín <78743513+lmtreser@users.noreply.github.com>
2022-02-19 12:55:00 -0300

    Funcionalidad cantidad de noticias

--- webscrap.py
+++ webscrap.py
@@ -13,10 +13,19 @@ print(page)
 # Hacer una sopa de datos
 soup = BeautifulSoup(page.content, 'html.parser')
 
+#Pide la cantidad de filas ( 1 noticia = 2 filas)
+cantidadfilas = input("Ingrese Cantidad de Noticias que desea: ")
+qfilas=int(cantidadfilas)
+
 try:
     contenedor = soup.find('table', width="80%")
     tabla = contenedor.find_all('table', width="100%")
-    texto = str(tabla)
+    rows = contenedor.find_all("tr")
+    texto=""
+    for i in range (1,qfilas+2,2):
+        texto = texto + str(rows[i+1])
+except IndexError:
+    print("No se encontraron tantas Noticias. Intente con otra cantidad")
 except AttributeError:
     print("No se encuentra el contenido dentro del documento")
 else:
@@ -25,7 +34,7 @@ else:
     string = """<!DOCTYPE html><html lang="en"><head>
                 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
                 <meta charset="utf-8"><title>WEB SCRAPER</title><body>
-                <h2>""" + url + "</h2>" + texto + "</body></html>"
+                <h2>""" + url + "</h2>" +  texto +"</body></html>"
     f.write(string)
     f.close()
 
